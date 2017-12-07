76e4a0dd2a7d2ebbfb6578117d7785df83799949
Francisco Trejo <trejo@Franciscos-MacBook-Air.local>
2017-12-07 04:36:05 -0700

    ultimo

--- app/src/main/java/com/example/carloszamora/carchoice/Resultado.java
+++ app/src/main/java/com/example/carloszamora/carchoice/Resultado.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.carloszamora.carchoice.APIConsumer.RestApiClient;
 import com.example.carloszamora.carchoice.Adapters.ResultadosAdapter;
@@ -24,27 +25,46 @@ public class Resultado extends AppCompatActivity {
     TextView cargando;
     ProgressBar progreso;
     ArrayList<Propuesta> optionsArray;
+    JSONObject infoUsuario;
+
+    String usuario;
+    String doors;
+    String cyl;
+    String seats;
+    String body;
+    String URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
 
+        infoUsuario = Global.jsonRespuesta;
+        try {
+            usuario = infoUsuario.getString("nombre");
+            doors = infoUsuario.getString("puertas");
+            cyl = infoUsuario.getString("motor");
+            seats = infoUsuario.getString("plazas");
+            body = infoUsuario.getString("gama");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         consultaAPI();
     }
 
     public void consultaAPI(){
-        String param1 = Global.param1+"";
+        String param1 = Global.param1+doors;
         String param2 = Global.param2;
-        String param3 = Global.param3+"";
-        String param4 = Global.param4+"";
+        String param3 = Global.param3+seats;
+        String param4 = Global.param4+cyl;
         String param5 = Global.param5;
-        String param6 = Global.param6+"";
+        String param6 = Global.param6+body;
         optionsArray = new ArrayList<>();
-        String URL = ""+param1+param2+param3+param4+param5+param6;
+        URL = ""+param1+param2+param3+param4+param5+param6;
         loader = (AVLoadingIndicatorView)findViewById(R.id.avi_loader);
 
-        new RestApiClient(Global.URL_D ,null, null, null, RestApiClient.METHOD.GET, new RestApiClient.RestInterface() {
+        new RestApiClient(URL ,null, null, null, RestApiClient.METHOD.GET, new RestApiClient.RestInterface() {
 
             @Override
             public void onFinish(final String Result) {
@@ -73,6 +93,8 @@ public class Resultado extends AppCompatActivity {
 
                     } catch (JSONException e) {
                         e.printStackTrace();
+                        Toast toast = Toast.makeText(getApplicationContext(),"Ups, lo siento, "+usuario+" creo que no encontre ninguna opcion para ti! :(",Toast.LENGTH_SHORT);
+                        toast.show();
                     }
                 }else{
                     stopAnim();
